a8c744b533cd843086700842d813532ecf12d5df
devopsrudr <119250929+devopsrudr@users.noreply.github.com>
2023-07-25 17:35:51 +0530

    Create programminglanguage.py

---
+++ programminglanguage.py
@@ -0,0 +1,36 @@
+print("Content that you wanna print on screen")
+#output
+var1 = "Shruti"
+print("Hi my name is: ",var1)
+#input from users
+var1 = input("Enter your name: ")
+print("My name is: ", var1)
+#typecasting input
+var1=int(input("enter the integer value"))
+print(var1)
+
+var1=float(input("enter the float value"))
+print(var1)
+
+#range functions
+#range(int_start_value,int_stop_value,int_step_value)
+
+for i in range(0,101,2):
+    print(i)
+
+for i in range(0,101,1):
+    print(i)
+
+for i in range(0,101,3):
+    print(i)
+
+#octal numbers
+print("\ooo")
+
+#string ,it can be used by both type of quotation single quotes and double quotes
+str="Shruti"
+print("string is ",str)
+
+#slicing
+str[1:5]
+print(str[1:5])
